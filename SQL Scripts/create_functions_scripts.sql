e9a2a6a360114808ec7be899e2ba5651ae8f62a4
tylerjacobpohlman <tylerjacobpohlman@gmail.com>
2023-04-11 17:39:50 -0400

    added functions to reduce clutter on inserts

---
+++ SQL Scripts/create_functions_scripts.sql
@@ -0,0 +1,114 @@
+USE hvs;
+
+-- Drops the previous version if it exists
+DROP FUNCTION IF EXISTS detailsDiscount;
+DELIMITER //
+CREATE FUNCTION detailsDiscount(
+    receipt_id_search INT,
+    item_id_search INT
+)
+RETURNS DECIMAL(2,2)
+DETERMINISTIC
+BEGIN
+	DECLARE discount DECIMAL(2,2);
+    
+    -- subquery to grab member_id
+    IF (SELECT member_id FROM receipts WHERE receipt_id = receipt_id_search) IS NOT NULL THEN
+		-- subquery if true, sets the discount to the item discount
+		SET discount = (SELECT item_discount_percentage FROM items WHERE item_id = item_id_search);
+    ELSEIF (SELECT member_id FROM receipts WHERE receipt_id = receipt_id_search) IS NULL THEN
+		-- sets the discount percentage to 0% if false
+		SET discount = 0.00;
+    END IF;
+	
+    RETURN(discount);
+END //
+DELIMITER ;
+
+-- *WARNING* must create detailsDiscount procedure first
+DROP FUNCTION IF EXISTS detailsPrice;
+DELIMITER //
+CREATE FUNCTION detailsPrice(
+    receipt_id_search INT,
+    item_id_search INT
+)
+RETURNS DECIMAL(9,2)
+DETERMINISTIC
+BEGIN
+    DECLARE price DECIMAL(9,2);
+
+    -- calculates the price with the given discount
+    -- subquery grabs the price of the given item
+    SET price = (SELECT item_price FROM items WHERE item_id = item_id_search) * (1 + detailsDiscount(receipt_id_search, item_id_search) );
+    
+    RETURN(price);
+
+END //
+DELIMITER ;
+
+DROP FUNCTION IF EXISTS detailsMemberID;
+DELIMITER //
+CREATE FUNCTION detailsMemberID(
+    receipt_id_search INT
+)
+RETURNS INT
+DETERMINISTIC
+BEGIN
+    DECLARE id INT;
+
+    SET id = (SELECT member_id FROM receipts WHERE receipt_id = receipt_id_search);
+
+    RETURN(id);
+END //
+DELIMITER ;
+    
+
+DROP FUNCTION IF EXISTS receiptsStateName;
+DELIMITER //
+CREATE FUNCTION receiptsStateName(
+    receipt_id_search INT
+)
+RETURNS CHAR(2)
+DETERMINISTIC
+BEGIN
+    DECLARE state CHAR(2);
+    
+    SET state = 
+    (
+    SELECT state_name 
+    FROM states 
+        JOIN stores ON states.state_name = stores.store_state
+        JOIN registers ON stores.store_id = registers.store_id
+        JOIN receipts ON registers.register_id = receipts.register_id
+    WHERE receipt_id = receipt_id_search
+    );
+    
+    RETURN(state);
+END //
+DELIMITER ;
+
+DROP FUNCTION IF EXISTS receiptsCashierName;
+DELIMITER //
+CREATE FUNCTION receiptsCashierName(
+    register_id_search INT,
+    given_datetime DATETIME
+)
+RETURNS VARCHAR(128)
+DETERMINISTIC
+BEGIN
+    DECLARE name VARCHAR(128);
+
+    SET name = 
+    (
+    SELECT CONCAT(cashier_first_name, ' ', cashier_last_name)
+    -- joins the cashiers and cashier_assignments tables
+    FROM cashier_assignments ca JOIN cashiers c ON ca.cashier_id = c.cashier_id
+    -- checks if the date is during a previous assignment range, or checks if the date is during a current assignment
+    WHERE ( (given_date BETWEEN assignment_from AND assignment_to) OR (given_date > assignment_from AND assignment_to IS NULL) )
+    AND register_id = register_id_search
+    );
+
+    RETURN(name)
+END //
+DELIMITER ;
+
