5248962867df3bc280040090667dc635fd594f4b
tylerjacobpohlman <tylerjacobpohlman@gmail.com>
2023-04-11 17:47:28 -0400

    added functions and cleaned up inserts

--- SQL Scripts/create_database_hvs.sql
+++ SQL Scripts/create_database_hvs.sql
@@ -3,9 +3,24 @@
 -- ***********************************
 DROP DATABASE IF EXISTS hvs;
 CREATE DATABASE hvs;
+
 USE hvs;
 
+-- *************************
+-- CREATE TABLES AND INDEXES
+-- *************************
+-- CREATES TABLE STATES
+-- This table is used for state taxes.
+CREATE TABLE states
+(
+    -- can act as the primary key since CHAR(2) isn't overly complex
+    -- also, makes it easier for inputting state into stores table
+	state_name CHAR(2) PRIMARY KEY,
+    -- each state must have a specified tax percentage
+    state_tax_percentage DECIMAL(2,2) NOT NULL
+);
 -- CREATES THE STORES TABLE
+-- *WARNING* must create table states first
 CREATE TABLE stores
 (
 	store_id INT PRIMARY KEY AUTO_INCREMENT,
@@ -14,13 +29,13 @@ CREATE TABLE stores
     store_number CHAR(5) NOT NULL UNIQUE,
     store_address VARCHAR(50),
     store_city VARCHAR(50),
-	-- for state taxes, a state MUST be provided
+	-- for state taxes
     store_state CHAR(2) NOT NULL,
     store_zip VARCHAR(20),
 	-- no 2 stores can have the same number, and phone number is numerically important 
-    store_phone VARCHAR(20) UNIQUE
+    store_phone VARCHAR(20) UNIQUE,
+    CONSTRAINT stores_fk_states FOREIGN KEY (store_state) REFERENCES states(state_name)
 );
-
 -- CREATES THE CASHIERS TABLE
 -- 	*WARNING* must create stores table first
 CREATE TABLE cashiers
@@ -70,7 +85,6 @@ CREATE TABLE cashier_assignments
     CONSTRAINT details_fk_cashiers FOREIGN KEY (cashier_id) REFERENCES cashiers(cashier_id),
     CONSTRAINT details_fk_registers FOREIGN KEY (register_id) REFERENCES registers(register_id)
 );
-
 -- CREATES TABLE ITEMS
 CREATE TABLE items
 (
@@ -89,7 +103,6 @@ CREATE TABLE items
 -- items are scanned in order to pull it up from the table, so this index speeds up that process
 CREATE INDEX idx_upc
 ON items (item_upc);
-
 -- CREATES TABLE MEMEBRS
 -- Basically, this stores whomever rewards members are. Rewards are able to access the given savings, 
 -- while nonmembers always pay the full price.
@@ -112,17 +125,6 @@ ON members (member_phone_number);
 -- membership are also scanned in, which is basically just the account number
 CREATE INDEX idx_account
 ON members (member_account_number);
-
--- CREATES TABLE STATES
--- This table is used for state taxes.
-CREATE TABLE states
-(
-	-- the table doesn't store much detail, and state_name is needed to compare to the receipt, so I made it the primary key
-	state_name CHAR(2) PRIMARY KEY,
-    -- each state must have a specified tax percentage
-    state_tax_percentage DECIMAL(2,2) NOT NULL
-);
-
 -- CREATES RECEIPTS TABLE
 -- *WARNING* must create registers, states, and members tables first
 CREATE TABLE receipts
@@ -130,7 +132,6 @@ CREATE TABLE receipts
 	receipt_id INT PRIMARY KEY AUTO_INCREMENT,
     -- foreign keys
     register_id INT NOT NULL,
-    state_name CHAR(2) NOT NULL,
     -- foreign key, but can be null if customer isn't a member
     member_id INT,
     -- each receipt has a unique number, and it must have that number
@@ -143,12 +144,10 @@ CREATE TABLE receipts
     receipt_change_due DECIMAL(9,2) DEFAULT 0.0,
     -- could use JOIN statements to get the same info, but indexing the database takes more time--i.e.,
     -- might as well store this value here too since it's unchanging
-    receipt_cashier_full_name VARCHAR(64) NOT NULL,
+    receipt_cashier_full_name VARCHAR(128) NOT NULL,
     CONSTRAINT receipts_fk_registers FOREIGN KEY (register_id) REFERENCES registers(register_id),
-    CONSTRAINT receipts_fk_states FOREIGN KEY (state_name) REFERENCES states(state_name),
     CONSTRAINT receipts_fk_members FOREIGN KEY (member_id) REFERENCES members(member_id)
 );
-
 -- CREATES RECEIPT_DETAILS TABLE
 -- *WARNING* must create receipts table first
 CREATE TABLE receipt_details
@@ -172,9 +171,135 @@ CREATE TABLE receipt_details
     CONSTRAINT pk_receipt_details PRIMARY KEY (receipt_id, item_id)
 );
 
+-- *****************
+-- CREATE FUNCTIONS
+-- *****************
+-- *WARNING* must create these procedures before complex insert statements
+-- creates detailsDiscount procedure used in the receipt_details table
+
+-- Drops the previous version if it exists
+DROP FUNCTION IF EXISTS detailsDiscount;
+DELIMITER //
+CREATE FUNCTION detailsDiscount(
+    receipt_id_search INT,
+    item_id_search INT
+)
+RETURNS DECIMAL(2,2)
+DETERMINISTIC
+BEGIN
+	DECLARE discount DECIMAL(2,2);
+    
+    -- subquery to grab member_id
+    IF (SELECT member_id FROM receipts WHERE receipt_id = receipt_id_search) IS NOT NULL THEN
+		-- subquery if true, sets the discount to the item discount
+		SET discount = (SELECT item_discount_percentage FROM items WHERE item_id = item_id_search);
+    ELSEIF (SELECT member_id FROM receipts WHERE receipt_id = receipt_id_search) IS NULL THEN
+		-- sets the discount percentage to 0% if false
+		SET discount = 0.00;
+    END IF;
+	
+    RETURN(discount);
+END //
+DELIMITER ;
+
+-- *WARNING* must create detailsDiscount procedure first
+DROP FUNCTION IF EXISTS detailsPrice;
+DELIMITER //
+CREATE FUNCTION detailsPrice(
+    receipt_id_search INT,
+    item_id_search INT
+)
+RETURNS DECIMAL(9,2)
+DETERMINISTIC
+BEGIN
+    DECLARE price DECIMAL(9,2);
+
+    -- calculates the price with the given discount
+    -- subquery grabs the price of the given item
+    SET price = (SELECT item_price FROM items WHERE item_id = item_id_search) * (1 + detailsDiscount(receipt_id_search, item_id_search) );
+    
+    RETURN(price);
+
+END //
+DELIMITER ;
+
+DROP FUNCTION IF EXISTS detailsMemberID;
+DELIMITER //
+CREATE FUNCTION detailsMemberID(
+    receipt_id_search INT
+)
+RETURNS INT
+DETERMINISTIC
+BEGIN
+    DECLARE id INT;
+
+    SET id = (SELECT member_id FROM receipts WHERE receipt_id = receipt_id_search);
+
+    RETURN(id);
+END //
+DELIMITER ;
+    
+
+DROP FUNCTION IF EXISTS receiptsStateTax;
+DELIMITER //
+CREATE FUNCTION receiptsStateTax(
+    receipt_id_search INT
+)
+RETURNS DECIMAL(2,2)
+DETERMINISTIC
+BEGIN
+    DECLARE tax DECIMAL(2,2);
+    
+    SET tax = 
+    (
+    SELECT state_tax_percentage 
+    FROM states 
+        JOIN stores ON states.state_name = stores.store_state
+        JOIN registers ON stores.store_id = registers.store_id
+        JOIN receipts ON registers.register_id = receipts.register_id
+    WHERE receipt_id = receipt_id_search
+    );
+
+    RETURN(tax);
+END //
+DELIMITER ;
+
+DROP FUNCTION IF EXISTS receiptsCashierName;
+DELIMITER //
+CREATE FUNCTION receiptsCashierName(
+    register_id_search INT,
+    given_datetime DATETIME
+)
+RETURNS VARCHAR(128)
+DETERMINISTIC
+BEGIN
+    DECLARE name VARCHAR(128);
+
+    SET name = 
+    (
+    SELECT CONCAT(cashier_first_name, ' ', cashier_last_name)
+    -- joins the cashiers and cashier_assignments tables
+    FROM cashier_assignments ca JOIN cashiers c ON ca.cashier_id = c.cashier_id
+    -- checks if the date is during a previous assignment range, or checks if the date is during a current assignment
+    WHERE ( (given_datetime BETWEEN assignment_from AND assignment_to) OR (given_datetime > assignment_from AND assignment_to IS NULL) )
+    AND register_id = register_id_search
+    );
+
+    RETURN(name);
+END //
+DELIMITER ;
+
+
+
 -- *******************************************************************************************
 -- The following have simple insert statements since they lack concrete foreign key restraints
 -- *******************************************************************************************
+INSERT INTO states
+VALUES
+('OH', 0.08),
+('KY', 0.04),
+('NY', 0.10)
+;
 INSERT INTO stores (store_number, store_address, store_city, store_state, store_zip, store_phone)
 VALUES 
 ('3329', '11706 Clifton Boulevard 117th & Clifton', 'Lakewood', 'OH', '44107', '(216) 228-9296'),
@@ -202,12 +327,6 @@ VALUES
 (2, '3452', 'Clerk'),
 (3, '1234', 'Clerk')
 ;
-INSERT INTO states
-VALUES
-('OH', 0.08),
-('KY', 0.04),
-('NY', 0.10)
-;
 INSERT INTO items (item_upc, item_name, item_price, item_discount_percentage)
 VALUES 
 ('4334523', 'Sprite Zero Lemon-Lime Soda 20 fl oz', 2.59, 0.0),
@@ -228,99 +347,20 @@ VALUES
 (5, 4, '2023-04-08 13:00:00', '2023-04-12 20:59:59')
 ;
 
--- ***********************************************************************************************
--- The following insert statements are complex since I plan to use them for a cashier Java program
--- ***********************************************************************************************
-INSERT INTO receipts (register_id, state_name, member_id, receipt_number, receipt_date_time, receipt_cashier_full_name)
+-- *************************
+-- COMPLEX INSERT STATEMENTS
+-- *************************
+INSERT INTO receipts (register_id, member_id, receipt_number, receipt_date_time, receipt_cashier_full_name)
 VALUES
-(
-1,
--- sets the state equal to the state of the store the register is assigned to
-(SELECT store_state FROM stores s JOIN registers r ON s.store_id = r.store_id WHERE register_id = 1),
-1,
-'67544567',
-'2023-01-01 22:10:26',
-(
--- 
-SELECT CONCAT(cashier_first_name, ' ', cashier_last_name)
--- joins the cashiers and cashier_assignments tables
-FROM cashier_assignments ca JOIN cashiers c ON ca.cashier_id = c.cashier_id
--- checks if the date is during a previous assignment range, or checks if the date is during a current assignment
--- WARNING: reach register can only have one cashier on it during a specific time period
-WHERE ( ('2023-01-01 22:10:26' BETWEEN assignment_from AND assignment_to) OR ('2023-01-01 22:10:26' > assignment_from AND assignment_to IS NULL) )
-AND register_id = 1
-)
-),
-(
-4,
-(SELECT store_state FROM stores s JOIN registers r ON s.store_id = r.store_id WHERE register_id = 4),
-NULL,
-'444237778',
-'2023-04-08 13:05:00',
-(
-SELECT CONCAT(cashier_first_name, ' ', cashier_last_name)
-FROM cashier_assignments ca JOIN cashiers c ON ca.cashier_id = c.cashier_id
-WHERE ( ('2023-04-08 13:05:00' BETWEEN assignment_from AND assignment_to) OR ('2023-04-08 13:05:00' > assignment_from AND assignment_to IS NULL) )
-AND register_id = 4
-)
-),
-(2,
-(SELECT store_state FROM stores s JOIN registers r ON s.store_id = r.store_id WHERE register_id = 1),
-3,
-'444238578',
-NOW(),
-(
-SELECT CONCAT(cashier_first_name, ' ', cashier_last_name)
-FROM cashier_assignments ca JOIN cashiers c ON ca.cashier_id = c.cashier_id
-WHERE  ( (NOW() BETWEEN assignment_from AND assignment_to) OR (NOW() > assignment_from AND assignment_to IS NULL) )
-AND register_id = 2
-)
-);
+(1, 1, '67544567','2023-01-01 22:10:26', receiptsCashierName(1, '2023-01-01 22:10:26') ),
+(4, NULL, '444237778','2023-04-08 13:05:00', receiptsCashierName(4, '2023-04-08 13:05:00') ),
+(2, 3, '444238578', NOW(), receiptsCashierName(2, NOW() ) );
 
 INSERT INTO receipt_details (receipt_id, item_id, member_id, item_discount_percentage, item_price, item_quantity)
 VALUES
-(
-1, 
-2,
--- can be null if the customer doesn't have a membership
-(SELECT member_id FROM receipts WHERE receipt_id = 1),
--- copies the discount percentage value to receipt_details if a member id was given in the receipt
-IF( (SELECT member_id FROM receipts WHERE receipt_id = 1) IS NOT NULL,
-	(SELECT item_discount_percentage FROM items WHERE item_id = 2),
-    0.00),
--- applies the discount if there's a member id, otherwise the price is copied from the items table
-IF( (SELECT member_id FROM receipts WHERE receipt_id = 1) IS NOT NULL, 
-	(SELECT item_price FROM items WHERE item_id = 2) * (1 - (SELECT item_discount_percentage FROM items WHERE item_id = 2) ),
-    (SELECT item_price FROM items WHERE item_id = 2)
-    ),
-2
-),
-(
-	1,
-    1,
-    (SELECT member_id FROM receipts WHERE receipt_id = 1),
-    IF( (SELECT member_id FROM receipts WHERE receipt_id = 1) IS NOT NULL,
-	(SELECT item_discount_percentage FROM items WHERE item_id = 1),
-    0.00),
-    IF( (SELECT member_id FROM receipts WHERE receipt_id = 1) IS NOT NULL, 
-	(SELECT item_price FROM items WHERE item_id = 1) * (1 - (SELECT item_discount_percentage FROM items WHERE item_id = 1) ),
-    (SELECT item_price FROM items WHERE item_id = 1)
-    ),
-    5
-),
-(
-2,
-3,
-(SELECT member_id FROM receipts WHERE receipt_id = 2),
-IF( (SELECT member_id FROM receipts WHERE receipt_id = 2) IS NOT NULL,
-	(SELECT item_discount_percentage FROM items WHERE item_id = 3),
-    0.00),
-IF( (SELECT member_id FROM receipts WHERE receipt_id = 2) IS NOT NULL, 
-	(SELECT item_price FROM items WHERE item_id = 3) * (1 - (SELECT item_discount_percentage FROM items WHERE item_id = 3) ),
-    (SELECT item_price FROM items WHERE item_id = 3)
-    ),
-    4
-)
+(1, 2, detailsMemberID(1), detailsDiscount(1, 2), detailsPrice(1, 2), 2),
+(1, 1, detailsMemberID(1), detailsDiscount(1, 1), detailsPrice(1, 1), 5),
+(2, 3, detailsMemberID(2), detailsDiscount(2, 3), detailsPrice(2, 3), 4)
 ;
 
 -- could have done in the insert statement, but it would have gotten too long
@@ -334,9 +374,8 @@ SET receipt_subtotal = (SELECT SUM(item_total) FROM receipt_details WHERE receip
 ;
 
 -- sets the total equal to the the subtotal plus state tax
-UPDATE receipts, states
-SET receipt_total = receipt_subtotal * (1 + state_tax_percentage)
-WHERE receipts.state_name = states.state_name
+UPDATE receipts
+SET receipt_total = receipt_subtotal * (1 + receiptsStateTax(receipt_id) )
 ;
 
 -- I'm lazy, so I assumed all the receipts were paid for in the exact amount
@@ -348,7 +387,3 @@ SET receipt_charge = receipt_total
 UPDATE receipts
 SET receipt_change_due = receipt_charge - receipt_total
 ;
-
-
-
-
